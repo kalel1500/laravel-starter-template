cb78c0d2028365f09ce28d0dd890c4b4e6e957ae
Adrian Canals <a.canals14@gmail.com>
2024-05-29 20:59:47 +0200

    Migrar a TypeScript v1

---
+++ resources/js/app.ts
@@ -0,0 +1,7 @@
+// Importamos e iniciamos los paquetes necesarios en la aplicacion
+import './bootstrap';
+
+// @ts-ignore
+import.meta.glob([
+    '../images/**',
+]);
---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import {defineConfig, loadEnv} from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+type Mode = 'production' | 'develop'
+
+export default ({ mode }: { mode: Mode }) => {
+    process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
+    const envIsLocal = (process.env.VITE_APP_ENV === 'local');
+    const envIsNotLocal = !envIsLocal;
+    const base = (process.env.VITE_USE_APPNAME_IN_SOURCE_PATH === 'true' && (mode === 'production'))
+        ? `/${process.env.VITE_APP_NAME_SHORT?.toLocaleLowerCase()}/build`
+        : undefined;
+
+    return defineConfig({
+        plugins: [
+            laravel({
+                input: ['resources/css/app.scss', 'resources/js/app.ts'],
+                refresh: false,
+            }),
+        ],
+        base: base,
+        build: {
+            minify: envIsNotLocal,
+            target: "es2022",
+            rollupOptions: {
+                output: {
+                    manualChunks: id => {
+                        if (id.includes('node_modules')) {
+                            return 'vendor';
+                        }
+                    }
+                }
+            }
+        },
+        css: {
+            devSourcemap: true
+        },
+    });
+}
