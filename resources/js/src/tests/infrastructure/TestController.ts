c16f76cc990e1d596e2558f16ef20e088eed8021
Adrian Canals <a.canals14@gmail.com>
2024-06-07 12:52:31 +0200

    Nuevo TestController.ts para pintar las constantes y traducciones

---
+++ resources/js/src/tests/infrastructure/TestController.ts
@@ -0,0 +1,24 @@
+import {Test} from "laravel-ts-utilities";
+import {_const} from "../../../app/constants";
+import {__} from "../../../app/translations";
+
+export default class TestController
+{
+    testConstants()
+    {
+        Test.printConstant();
+        console.log('-------------------------')
+        console.log(_const('lang'));
+        console.log(_const('anotherSetting'));
+
+        console.log('-------------------------')
+        console.log('-------------------------')
+        console.log('-------------------------')
+
+        Test.printTranslation();
+        console.log('-------------------------')
+        console.log(__('contact_pi_team'))
+        console.log(__('test_message'))
+        console.log(__('qqq'))
+    }
+}
