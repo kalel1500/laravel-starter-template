0db8c3961b6929e7e5109a0d4e0a7c9ea3e7d0f6
Adrian Canals <a.canals14@gmail.com>
2024-06-07 12:46:14 +0200

    Pasar tipo Translator en el getInstance (Translator.getInstance<AppTranslations>()) + refactor const __() inline

--- resources/js/app/translations.ts
+++ resources/js/app/translations.ts
@@ -7,9 +7,8 @@ export interface AppTranslations extends DefaultTranslations {
     "test_message_VARIABLE": string
 }
 
-const translator = Translator.getInstance() as Translator<AppTranslations>;
+const translator = Translator.getInstance<AppTranslations>();
 translator.registerTranslations('es', es);
 translator.registerTranslations('en', en);
-export const __ = (key: keyof AppTranslations, replacements?: Record<string, string>) => {
-    return translator.get(key, replacements);
-}
+
+export const __ = (key: keyof AppTranslations, replacements?: Record<string, string>) => translator.get(key, replacements);
