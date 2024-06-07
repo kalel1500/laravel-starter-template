c5c90d2baed7c1c034f795ebcd3efa88db95debd
Adrián <acanals@externos.itnow.es>
2024-06-07 12:52:31 +0200

    Nuevo TestController.ts para pintar las constantes y traducciones

--- resources/js/src/home/infrastructure/HomeController.ts
+++ resources/js/src/home/infrastructure/HomeController.ts
@@ -1,13 +1,9 @@
-import {__} from "../../../app/translations";
 import {_const} from "../../../app/constants";
 
 export default class HomeController
 {
     index()
     {
-        console.log(_const.anotherSetting)
-        console.log(__('contact_pi_team'))
-        console.log(__('test_message'))
-        console.log(__('test_message_VARIABLE', {variable: 'qqqq'}))
+        console.log(_const('VITE_BROADCASTING_ENABLED'))
     }
 }
