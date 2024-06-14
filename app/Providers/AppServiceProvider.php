ef98bb30e25d0a0bf908e5ddad3dc27f4fdab591
Adrián <acanals@externos.itnow.es>
2024-06-14 19:05:54 +0200

    mejoras varias (bootstrap/app.php)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,9 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        // HexagonalService::ignoreMigrations();
     }
 
     /**
@@ -19,6 +22,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+//        RateLimiter::for('startAsyncAction', function (Request $request) {
+//            return Limit::perMinute(1)->by($request->path());
+//            // return $request->is('*/status/*') ? Limit::perMinute(1)->by($request->path()) : Limit::none();
+//        });
     }
 }
