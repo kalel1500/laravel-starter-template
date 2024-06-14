f8ddb8553bcdfe55240ca431dff3f3b8f1f64000
Adrian Canals <a.canals14@gmail.com>
2024-06-14 19:05:54 +0200

    mejoras varias (bootstrap/app.php)

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,19 +1,17 @@
 <?php
 
 use Illuminate\Foundation\Application;
-use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+use Thehouseofel\Hexagonal\Infrastructure\Exceptions\ExceptionHandler;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         web: __DIR__.'/../routes/web.php',
         commands: __DIR__.'/../routes/console.php',
-        channels: __DIR__.'/../routes/channels.php',
         health: '/up',
     )
+    ->withExceptions(ExceptionHandler::getUsingCallback())
     ->withMiddleware(function (Middleware $middleware) {
         //
     })
-    ->withExceptions(function (Exceptions $exceptions) {
-        //
-    })->create();
+    ->create();
