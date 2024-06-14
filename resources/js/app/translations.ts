a38905fbbb8d4a3ae49965342177ec46fded661f
Adrian Canals <a.canals14@gmail.com>
2024-06-14 18:26:16 +0200

    Tipar el parametro replacements en las traducciones

--- resources/js/app/translations.ts
+++ resources/js/app/translations.ts
@@ -1,6 +1,6 @@
 import es from "./lang/es.json";
 import en from "./lang/en.json";
-import {DefaultTranslations, Translator} from "laravel-ts-utilities";
+import {DefaultTranslations, TranslationReplacements, Translator} from "laravel-ts-utilities";
 
 export interface AppTranslations extends DefaultTranslations {
     "test_message": string;
@@ -11,4 +11,4 @@ const translator = Translator.getInstance<AppTranslations>();
 translator.registerTranslations("es", es);
 translator.registerTranslations("en", en);
 
-export const __ = (key: keyof AppTranslations, replacements?: Record<string, string>) => translator.get(key, replacements);
+export const __ = (key: keyof AppTranslations, replacements?: TranslationReplacements) => translator.get(key, replacements);
