dd6b90377196573d32058ba64fbb8feddfb749f3
Adrian Canals <a.canals14@gmail.com>
2024-08-14 22:54:18 +0200

    Nueva ruta y controller "testJsonErrors" para probar todos los tipos de errores que devuelven las apis

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use Src\Home\Infrastructure\HomeController;
+use Src\Tests\Infrastructure\TestController;
 
-Route::get('/', function () {
-    return redirect()->route('home.index');
-});
+Route::get('/', fn() => redirect()->route('home.index'));
 
 Route::get('/home', [HomeController::class, 'index'])->name('home.index');
+
+Route::get('/testJsonErrors', [TestController::class, 'testJsonErrors']);
