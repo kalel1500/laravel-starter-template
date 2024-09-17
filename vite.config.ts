63e3406eebd20320a909431e5086adf3ca99799b
Adrian Canals <a.canals14@gmail.com>
2024-09-17 11:59:42 +0200

    mejorarPlantilla08: mejorar uso variables en "vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,18 @@
 import {defineConfig, loadEnv} from 'vite';
 import laravel from 'laravel-vite-plugin';
 
-type Mode = 'production' | 'develop'
-
-export default ({ mode }: { mode: Mode }) => {
+export default ({ mode }: { mode: string }) => {
     process.env = Object.assign(process.env, loadEnv(mode, process.cwd(), ''));
-    const envIsLocal = (process.env.VITE_APP_ENV === 'local');
+
+    const env = process.env.APP_ENV ?? 'local'
+    const useAppNameInSourcePath = (process.env.VITE_USE_APPNAME_IN_SOURCE_PATH === 'true')
+
+    const envIsLocal = env === 'local';
     const envIsNotLocal = !envIsLocal;
-    const base = (process.env.VITE_USE_APPNAME_IN_SOURCE_PATH === 'true' && (mode === 'production'))
-        ? `/${process.env.VITE_APP_NAME_SHORT?.toLocaleLowerCase()}/build`
-        : undefined;
+    const modeIsProd = mode === 'production';
+
+    const appName = process.env.VITE_APP_NAME_SHORT?.toLocaleLowerCase() ?? process.env.VITE_APP_NAME?.toLowerCase().replace(/(?:^\w|[A-Z]|\b\w|\s+|[_-])/g, (match, index) => index === 0 ? match.toLowerCase() : match.toUpperCase()).replace(/\s+|[_-]/g, '');
+    const base = (useAppNameInSourcePath && modeIsProd) ? `/${appName}/build` : undefined;
 
     return defineConfig({
         plugins: [
