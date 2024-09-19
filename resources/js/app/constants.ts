956900aff932a0113e19ad69faeb5f52c27fe9fb
Adrian Canals <a.canals14@gmail.com>
2024-09-19 16:27:37 +0200

    mejorarPlantilla15: ordenar constantes

--- resources/js/app/constants.ts
+++ resources/js/app/constants.ts
@@ -6,15 +6,17 @@ interface AppConstants extends DefaultConstants {
 
 const constants = Constants.getInstance<AppConstants>();
 constants.extend({
-    appIcon: new URL("/resources/images/favicon.ico", import.meta.url).href,
-    VITE_BROADCASTING_ENABLED: import.meta.env.VITE_BROADCASTING_ENABLED === "true",
-    VITE_REVERB_APP_KEY: import.meta.env.VITE_REVERB_APP_KEY,
-    VITE_REVERB_HOST: import.meta.env.VITE_REVERB_HOST,
-    VITE_REVERB_PORT: parseInt(import.meta.env.VITE_REVERB_PORT),
-    VITE_REVERB_SCHEME: import.meta.env.VITE_REVERB_SCHEME,
-    VITE_APP_NAME: import.meta.env.VITE_APP_NAME,
-    VITE_APP_ENV: import.meta.env.VITE_APP_ENV,
-    anotherSetting: "newCustomValue",
+    appIcon:                    new URL("/resources/images/favicon.ico", import.meta.url).href,
+
+    VITE_REVERB_APP_KEY:        import.meta.env.VITE_REVERB_APP_KEY,
+    VITE_REVERB_HOST:           import.meta.env.VITE_REVERB_HOST,
+    VITE_REVERB_PORT:           parseInt(import.meta.env.VITE_REVERB_PORT),
+    VITE_REVERB_SCHEME:         import.meta.env.VITE_REVERB_SCHEME,
+
+    VITE_BROADCASTING_ENABLED:  import.meta.env.VITE_BROADCASTING_ENABLED === "true",
+    VITE_APP_ENV:               import.meta.env.VITE_APP_ENV,
+    VITE_APP_NAME:              import.meta.env.VITE_APP_NAME,
+    anotherSetting:             "newCustomValue",
 });
 
 export const _const = <T extends keyof AppConstants>(key: T): AppConstants[T] => constants.get(key);
