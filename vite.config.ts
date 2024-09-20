bf54c64e45007b70a163417dfc3c3c1e1daa91c9
Adrián <acanals@externos.itnow.es>
2024-09-20 18:56:58 +0200

    mejorarPlantilla24: código "vite.config.ts" simplificado exportando directamente la configuración en lugar de la función (como estaba originalmente)

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,13 @@
-import {defineConfig} from 'vite';
+import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import {laravelTsUtilsPlugin} from "laravel-ts-utilities/plugins";
+import { laravelTsUtilsPlugin } from 'laravel-ts-utilities/plugins';
 
-export default () => {
-    return defineConfig({
-        plugins: [
-            laravel({
-                input: ['resources/css/app.css', 'resources/js/app.ts'],
-                refresh: false,
-            }),
-            laravelTsUtilsPlugin(),
-        ]
-    });
-}
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            refresh: false,
+        }),
+        laravelTsUtilsPlugin(),
+    ],
+});
