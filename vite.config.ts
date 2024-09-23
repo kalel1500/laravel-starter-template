cbb24689b4bfc9f06a18737f91033db808966a47
Adrian Canals <a.canals14@gmail.com>
2024-09-23 15:53:49 +0200

    Paquete "laravel-ts-utilities" actualizado (ruta "laravelTsUtilsPlugin()" modificada)

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { laravelTsUtilsPlugin } from 'laravel-ts-utilities/plugins';
+import { laravelTsUtilsPlugin } from 'laravel-ts-utilities/dist/plugins';
 
 export default defineConfig({
     plugins: [
