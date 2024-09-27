8f6f58840fa5c3e50fcaca271f0f52a9bb10f3dc
Adrián <acanals@externos.itnow.es>
2024-09-27 21:50:29 +0200

    estructuraBlades03: nuevo código global "SharedController@layout" para gestionar el modo oscuro

--- resources/js/app/routes.ts
+++ resources/js/app/routes.ts
@@ -1,8 +1,11 @@
 import { Route } from 'laravel-ts-utilities';
 import HomeController from '../src/home/infrastructure/HomeController';
 import TestController from '../src/shared/infrastructure/TestController';
+import SharedController from "../src/shared/infrastructure/SharedController";
 
 export function defineRoutes(): void {
+    Route.all([SharedController, 'layout'], []);
+
     Route.page('shared.test', [TestController, 'test']);
     Route.page('home.index', [HomeController, 'index']);
 }
