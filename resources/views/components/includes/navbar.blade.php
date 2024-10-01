33a201a3d5c4bcec13204fb405f677587e3db51d
Adrian Canals <a.canals14@gmail.com>
2024-10-01 10:35:08 +0200

    estructuraBlades08: navbar mejorado

--- resources/views/components/includes/navbar.blade.php
+++ resources/views/components/includes/navbar.blade.php
@@ -18,11 +18,11 @@
                 </a>
                 <form action="#" method="GET" class="hidden lg:block lg:pl-2">
                     <label for="topbar-search" class="sr-only">Search</label>
-                    <div class="relative mt-1 lg:w-96">
+                    <div class="relative lg:w-96">
                         <div class="flex absolute inset-y-0 left-0 items-center pl-3 pointer-events-none">
                             <svg class="w-4 h-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20"> <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z"/> </svg>
                         </div>
-                        <input type="text" name="email" id="topbar-search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-9 p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search">
+                        <input type="text" name="email" id="topbar-search" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full pl-9 p-1.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Search">
                     </div>
                 </form>
             </div>
