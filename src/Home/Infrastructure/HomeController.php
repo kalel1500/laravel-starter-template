dd2ed287336540039e1fc7b0d13a88706a52c73c
Adrian Canals <a.canals14@gmail.com>
2024-10-01 14:59:26 +0200

    estructuraBlades10: nueva ruta en HomeController con otro ejemplo de tailwind

--- src/Home/Infrastructure/HomeController.php
+++ src/Home/Infrastructure/HomeController.php
@@ -9,8 +9,13 @@ use Thehouseofel\Hexagonal\Infrastructure\Controllers\Controller;
 
 final class HomeController extends Controller
 {
-    public function index(): View
+    public function example1(): View
     {
-        return view('pages.home.index');
+        return view('pages.home.example1');
+    }
+
+    public function example2(): View
+    {
+        return view('pages.home.example2');
     }
 }
