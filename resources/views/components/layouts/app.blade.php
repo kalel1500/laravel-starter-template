45b7fcd58053ca98c0846d98b8233eda7f1235e2
Adrián <acanals@externos.itnow.es>
2024-10-02 17:56:31 +0200

    estructuraBlades23: slot $breadcrumb en el layout/app

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -37,7 +37,10 @@
             @php($mainClass = config('hexagonal.blade_show_main_border') ? 'border-2 border-dashed border-gray-300 dark:border-gray-600 p-2' : null)
             <main class="{{ $mainClass }}">
 
-                <!-- App mensajes -->
+                <!-- Page breadcrumb -->
+                {{ $breadcrumb ?? '' }}
+
+                <!-- Page mensajes -->
                 <x-includes.messages/>
 
                 <!-- Page content -->
