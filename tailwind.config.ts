f058bf0cb76d42c619da8f64f98ace310607ec3b
Adrián <acanals@externos.itnow.es>
2024-10-03 19:03:02 +0200

    estructuraBlades24: nueva variante de tailwind "sc" (sidebar collapse) para poder indicar en las clases "sc:..." igual que con el modo oscuro

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from 'tailwindcss'
+import plugin from 'tailwindcss/plugin';
 
 export default {
   content: [
@@ -16,7 +17,10 @@ export default {
     },
   },
   plugins: [
-    require('flowbite/plugin')
+    require('flowbite/plugin'),
+    plugin(function ({ addVariant }) {
+      addVariant('sc', '&:is(.sc *)');
+    })
   ],
 } satisfies Config
 
