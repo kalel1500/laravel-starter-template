fccd9079db1676a6b514c424d8b8044828378726
Adrian Canals <a.canals14@gmail.com>
2024-10-05 00:25:09 +0200

    estructuraBlades32: llenar componente de "<x-messages/>" con varios mensajes flash + nueva ruta "testFlash" para probar los mensajes

--- resources/views/components/messages/index.blade.php
+++ resources/views/components/messages/index.blade.php
@@ -1,3 +1,27 @@
-<div>
-    <!-- Simplicity is an acquired taste. - Katharine Gerould -->
-</div>
+@if($errors->any())
+    <x-alert.list id="alert-errors" color="red" title="{{ __('Several errors have been detected') }}:" xmlns:x-slot="http://www.w3.org/1999/xlink">
+        @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+        @endforeach
+    </x-alert.list>
+@endif
+
+@if(session()->has('success'))
+    <x-alert.simple id="alert-success" color="green">{{ session()->get('success') }}</x-alert.simple>
+@endif
+
+@if(session()->has('error'))
+    <x-alert.simple id="alert-success" color="red">{{ session()->get('error') }}</x-alert.simple>
+@endif
+
+@if(session()->has('severalErrors'))
+    <x-alert.list id="alert-errors" color="red">
+        @foreach(session()->get('severalErrors') as $key => $error)
+            @if($key === 0)
+                <x-slot:title>{{ $error }}</x-slot:title>
+            @else
+                <li>{{ $error }}</li>
+            @endif
+        @endforeach
+    </x-alert.list>
+@endif
