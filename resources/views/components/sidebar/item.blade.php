6d9e5b7165043cc8bde0dcb7efa7af58d92d45de
Adrian Canals <a.canals14@gmail.com>
2024-10-07 22:05:09 +0200

    estructuraBlades38: pasar sidebar a componentes v2 -> + nuevo componente "<x-sidebar.separator>" para separar enlaces con una linea + (fix) sidebar.item -> construir el "aria-controls" del "dropdown" con el parámetro id (aria-controls="dropdown-{{ $dropdownId }}") + sidebar.item -> mergear propiedad href + sidebar.item -> con un nuevo div con estilos para pintar el nuevo parámetro "counter" + añadir las clases "flex-1 whitespace-nowrap" si el link tiene contador

--- resources/views/components/sidebar/item.blade.php
+++ resources/views/components/sidebar/item.blade.php
@@ -1,4 +1,4 @@
-@props(['icon', 'sublinks'])
+@props(['icon', 'sublinks', 'counter'])
 
 @php
     $isDropdown = isset($sublinks);
@@ -11,7 +11,7 @@
 
 <li>
     @if($isDropdown)
-        <button type="button" class="{{ $linkClasses }}" aria-controls="dropdown-pages" data-collapse-toggle="dropdown-{{ $dropdownId }}">
+        <button type="button" class="{{ $linkClasses }}" aria-controls="dropdown-{{ $dropdownId }}" data-collapse-toggle="dropdown-{{ $dropdownId }}">
             {!! $iconHtml !!}
             <span @class([$spanClasses, 'flex-1 whitespace-nowrap text-left'])>{{ $slot }}</span>
             <svg aria-hidden="true" class="h-6 w-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
@@ -21,12 +21,15 @@
             {{ $sublinks }}
         </ul>
     @else
-        <a href="#" @class([$linkClasses, 'pl-11' => $isSublink,])>
+        <a {{ $attributes->only('href')->merge(['href' => '#']) }} @class([$linkClasses, 'pl-11' => $isSublink])>
             @if ($isSublink)
                 {{ $slot }}
             @else
                 {!! $iconHtml !!}
-                <span @class([$spanClasses])>{{ $slot }}</span>
+                <span @class([$spanClasses, 'flex-1 whitespace-nowrap' => isset($counter)])>{{ $slot }}</span>
+                @isset($counter)
+                    <span class="text-blue-800 bg-blue-100 dark:bg-blue-200 dark:text-blue-800 inline-flex h-5 w-5 items-center justify-center rounded-full text-xs font-semibold">{{ $counter }}</span>
+                @endisset
             @endif
         </a>
     @endif
