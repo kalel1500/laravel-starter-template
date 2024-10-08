9dca0090cecc556c8131ee2efe435b2c835438ba
Adrián <acanals@externos.itnow.es>
2024-10-08 13:48:25 +0200

    estructuraBlades47: pasar footer sidebar a componentes v3

---
+++ resources/views/components/sidebar/footer/subitem.blade.php
@@ -0,0 +1,7 @@
+<li>
+    <a href="#" class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:text-gray-300 dark:hover:bg-gray-600 dark:hover:text-white" role="menuitem">
+        <div class="inline-flex items-center">
+            {{ $slot }}
+        </div>
+    </a>
+</li>
