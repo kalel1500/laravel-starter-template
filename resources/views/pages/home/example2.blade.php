4692f916246047dd001a1597c9f7e1499e334d6f
Adrián <acanals@externos.itnow.es>
2024-10-08 11:44:39 +0200

    estructuraBlades43: pasar Breadcrumb a componentes

--- resources/views/pages/home/example2.blade.php
+++ resources/views/pages/home/example2.blade.php
@@ -1,5 +1,18 @@
 <x-layouts.app title="Example 1">
 
+    <x-slot:breadcrumb>
+        <x-breadcrumb place="end">
+            <x-breadcrumb.item first>
+                <x-slot:icon>
+                    <svg class="me-2.5 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                </x-slot:icon>
+                Home
+            </x-breadcrumb.item>
+            <x-breadcrumb.item>Users</x-breadcrumb.item>
+            <x-breadcrumb.item>Settings</x-breadcrumb.item>
+        </x-breadcrumb>
+    </x-slot>
+
     <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-4 mb-4">
         <div class="border-2 border-dashed border-gray-300 rounded-lg dark:border-gray-600 h-32 md:h-64"></div>
         <div class="border-2 border-dashed rounded-lg border-gray-300 dark:border-gray-600 h-32 md:h-64"></div>
