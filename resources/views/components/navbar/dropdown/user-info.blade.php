e524efca9c8832b7a4765e5bef51b0987908a3e6
Adrian Canals <a.canals14@gmail.com>
2024-10-11 16:12:53 +0200

    estructuraBlades61: pasar navbar a componentes v13 -> pasar User v4 (completo)

---
+++ resources/views/components/navbar/dropdown/user-info.blade.php
@@ -0,0 +1,4 @@
+@props(['name', 'email'])
+
+<span class="block text-sm font-semibold text-gray-900 dark:text-white">{{ $name }}</span>
+<span class="block truncate text-sm text-gray-900 dark:text-white">{{ $email }}</span>
