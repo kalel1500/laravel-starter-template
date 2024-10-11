ccacd58e906483dcf5eea1444e4f6d14918086c7
Adrian Canals <a.canals14@gmail.com>
2024-10-11 14:57:06 +0200

    estructuraBlades60: pasar navbar a componentes v12 -> pasar User v3 (unificar los componentes "<x-navbar.subitem>" "<x-navbar.subitem.list>" y "<x-navbar.subitem.square>" en el componente "<x-navbar).dropdown.link>"

--- resources/views/components/navbar/subitem/square.blade.php
+++
@@ -1,8 +0,0 @@
-@props(['href' => '#', 'text'])
-
-<a href="{{ $href }}" class="group block rounded-lg p-4 text-center hover:bg-gray-100 dark:hover:bg-gray-600">
-    <div class="mx-auto mb-1 h-7 w-7 text-gray-400 group-hover:text-gray-500 dark:text-gray-400 dark:group-hover:text-gray-400">
-        {{ $slot }}
-    </div>
-    <div class="text-sm text-gray-900 dark:text-white">{{ $text }}</div>
-</a>
