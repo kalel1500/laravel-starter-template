22a95459f9bc2c5c101b0728a99534166e6ad9d3
Adrián <acanals@externos.itnow.es>
2024-10-14 11:44:02 +0200

    estructuraBlades74: sidebarStatus -> nuevo código para manejar la clase "sc" igual que el modo oscuro para colapsar y expandir el sidebar

--- resources/js/src/shared/Application/DarkModeUseCase.ts
+++ resources/js/src/shared/Application/DarkModeUseCase.ts
@@ -4,10 +4,13 @@ export default class DarkModeUseCase extends Instantiable
 {
     __invoke()
     {
+        const $document = document.documentElement;
+
+        /* --- Dark mode ---*/
+
         const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
         const themeToggleBtn = document.getElementById('theme-toggle');
-        const $document = document.documentElement;
 
         // Función para cambiar el tema
         const setTheme = (theme: string) => {
@@ -33,5 +36,31 @@ export default class DarkModeUseCase extends Instantiable
             const currentTheme = $document.classList.contains('dark') ? 'light' : 'dark';
             setTheme(currentTheme);
         });
+
+        /* --- Sidebar status ---*/
+        const sidebarToggleBtn = document.getElementById('sidebar-toggle');
+
+        // Función para cambiar el estado del sidebar
+        const setSidebar = (sidebar: string) => {
+            const isCollapsed = sidebar === 'collapsed';
+            $document.classList.toggle('sc', isCollapsed);
+            localStorage.setItem('sidebar-state', sidebar);
+        };
+
+        // Comprobar tema inicial
+        const savedSidebar = localStorage.getItem('sidebar-state');
+
+        if (savedSidebar === 'collapsed') {
+            setSidebar('collapsed');
+        } else {
+            setSidebar('expanded');
+        }
+
+        // Evento de click para alternar el tema
+        sidebarToggleBtn?.addEventListener('click', () => {
+            const currentTheme = $document.classList.contains('sc') ? 'expanded' : 'collapsed';
+            setSidebar(currentTheme);
+        });
+
     }
 }
