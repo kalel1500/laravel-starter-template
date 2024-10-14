e4ba2d1e9242a5fa108dc317ab78f788e030eb3f
Adrián <acanals@externos.itnow.es>
2024-10-14 09:39:18 +0200

    estructuraBlades72: darkMode -> código mejorado

--- resources/js/src/shared/Application/DarkModeUseCase.ts
+++ resources/js/src/shared/Application/DarkModeUseCase.ts
@@ -4,52 +4,43 @@ export default class DarkModeUseCase extends Instantiable
 {
     __invoke()
     {
-        // On page load or when changing themes, best to add inline in `head` to avoid FOUC
-        if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            document.documentElement.classList.add('dark');
-        } else {
-            document.documentElement.classList.remove('dark');
-        }
-
-
         const themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
         const themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
         const themeToggleBtn = document.getElementById('theme-toggle');
+        const $document = document.documentElement;
 
-        if (themeToggleDarkIcon === null || themeToggleLightIcon === null || themeToggleBtn === null) {
-            return;
-        }
-
-        // Change the icons inside the button based on previous settings
+        // Add the "dark" class to the page and change the icons inside the button according to the previous settings
         if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
-            themeToggleLightIcon.classList.remove('hidden');
+            $document.classList.add('dark');
+            themeToggleLightIcon?.classList.remove('hidden');
         } else {
-            themeToggleDarkIcon.classList.remove('hidden');
+            $document.classList.remove('dark');
+            themeToggleDarkIcon?.classList.remove('hidden');
         }
 
-        themeToggleBtn.addEventListener('click', function() {
+        themeToggleBtn?.addEventListener('click', function() {
 
             // toggle icons inside button
-            themeToggleDarkIcon.classList.toggle('hidden');
-            themeToggleLightIcon.classList.toggle('hidden');
+            themeToggleDarkIcon?.classList.toggle('hidden');
+            themeToggleLightIcon?.classList.toggle('hidden');
 
             // if set via local storage previously
             if (localStorage.getItem('color-theme')) {
                 if (localStorage.getItem('color-theme') === 'light') {
-                    document.documentElement.classList.add('dark');
+                    $document.classList.add('dark');
                     localStorage.setItem('color-theme', 'dark');
                 } else {
-                    document.documentElement.classList.remove('dark');
+                    $document.classList.remove('dark');
                     localStorage.setItem('color-theme', 'light');
                 }
 
                 // if NOT set via local storage previously
             } else {
-                if (document.documentElement.classList.contains('dark')) {
-                    document.documentElement.classList.remove('dark');
+                if ($document.classList.contains('dark')) {
+                    $document.classList.remove('dark');
                     localStorage.setItem('color-theme', 'light');
                 } else {
-                    document.documentElement.classList.add('dark');
+                    $document.classList.add('dark');
                     localStorage.setItem('color-theme', 'dark');
                 }
             }
