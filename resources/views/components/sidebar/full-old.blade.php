c6f726b332ff1bf815c9bf42a71b435bb707c1c4
Adrián <acanals@externos.itnow.es>
2024-10-18 14:53:55 +0200

    estructuraBlades87: pasar sidebar a la configuración v1 -> renombrar full a full-old

--- resources/views/components/sidebar/full-old.blade.php
+++ resources/views/components/sidebar/full-old.blade.php
@@ -3,7 +3,7 @@
         <x-sidebar.search-from/>
     </x-slot:header>
 
-    <x-sidebar.item href="{{ route('shared.test') }}">
+    <x-sidebar.item href="{{ route('shared.testJs') }}">
         <x-slot:icon>
             <svg aria-hidden="true" class="h-6 w-6 flex-shrink-0 text-gray-500 transition duration-75 group-hover:text-gray-900 dark:text-gray-400 dark:group-hover:text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path><path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path></svg>
         </x-slot:icon>
