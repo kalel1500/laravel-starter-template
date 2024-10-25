0580e7ba404aa36f0291f5569106d2cd25491e4b
Adrián <acanals@externos.itnow.es>
2024-10-25 20:26:01 +0200

    mover componentes al paquete 04: adaptar blades

--- resources/views/pages/home/example1.blade.php
+++ resources/views/pages/home/example1.blade.php
@@ -1,16 +1,11 @@
-<x-layout.app title="Example 2">
+<x-hexagonal::layout.app title="Example 2">
 
     <x-slot:breadcrumb>
-        <x-breadcrumb>
-            <x-breadcrumb.item first>
-                <x-slot:icon>
-                    <svg class="me-2.5 h-5 w-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
-                </x-slot:icon>
-                Home
-            </x-breadcrumb.item>
-            <x-breadcrumb.item>Users</x-breadcrumb.item>
-            <x-breadcrumb.item>Settings</x-breadcrumb.item>
-        </x-breadcrumb>
+        <x-hexagonal::breadcrumb>
+            <x-hexagonal::breadcrumb.item first>Home</x-hexagonal::breadcrumb.item>
+            <x-hexagonal::breadcrumb.item>Users</x-hexagonal::breadcrumb.item>
+            <x-hexagonal::breadcrumb.item>Settings</x-hexagonal::breadcrumb.item>
+        </x-hexagonal::breadcrumb>
     </x-slot>
 
     <div class="p-4 border-2 border-gray-200 border-dashed rounded-lg dark:border-gray-700">
@@ -115,4 +110,4 @@
 
     </div>
 
-</x-layout.app>
+</x-hexagonal::layout.app>
--- resources/views/pages/home/example3.blade.php
+++ resources/views/pages/home/example3.blade.php
@@ -1,5 +1,5 @@
-<x-layout.app title="Example 3">
-    <x-section>
+<x-hexagonal::layout.app title="Example 3">
+    <x-hexagonal::section>
         <form>
             <div class="grid gap-6 mb-6 md:grid-cols-2">
                 <div>
@@ -47,5 +47,5 @@
             </div>
             <button type="submit" class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Submit</button>
         </form>
-    </x-section>
-</x-layout.app>
+    </x-hexagonal::section>
+</x-hexagonal::layout.app>
