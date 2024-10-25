6472a8f42e654e2e658738ef3243295c17183609
Adrian Canals <a.canals14@gmail.com>
2024-10-25 21:59:08 +0200

    simplificar: mover código tests y vista tailwind a la documentación de los paquetes "hexagonal" y "ts-utils" respectivamente + mover ruta testJs a example0

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use Src\Home\Infrastructure\HomeController;
-use Src\Shared\Infrastructure\TestController;
 
-Route::get('/', fn() => redirect()->route('home.example1'));
+Route::get('/', fn() => redirect()->route('home.example0'));
 
-Route::get('/compare',          [TestController::class, 'compare'])->name('shared.compare');
-Route::get('/test-js',          [TestController::class, 'testJs'])->name('shared.testJs');
-Route::get('/testJsonErrors',   [TestController::class, 'testJsonErrors']);
-Route::get('/tailwind',         [TestController::class, 'tailwind']);
-Route::get('/testFlash',        [TestController::class, 'testFlash']);
-
-Route::get('/home/example1', [HomeController::class, 'example1'])->name('home.example1');
-Route::get('/home/example2', [HomeController::class, 'example2'])->name('home.example2');
-Route::get('/home/example3', [HomeController::class, 'example3'])->name('home.example3');
+Route::get('/compare',       [\Src\Shared\Infrastructure\TestController::class, 'compare'])->name('shared.compare');
+Route::get('/home/example0', [\Src\Home\Infrastructure\HomeController::class, 'example0'])->name('home.example0');
+Route::get('/home/example1', [\Src\Home\Infrastructure\HomeController::class, 'example1'])->name('home.example1');
+Route::get('/home/example2', [\Src\Home\Infrastructure\HomeController::class, 'example2'])->name('home.example2');
+Route::get('/home/example3', [\Src\Home\Infrastructure\HomeController::class, 'example3'])->name('home.example3');
