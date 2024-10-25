7cbdf532e286bd4845349efb499fc15f476a6c19
Adrian Canals <a.canals14@gmail.com>
2024-10-25 20:27:11 +0200

    mover componentes al paquete 05: adaptar LayoutService y borrar Facade

--- src/Shared/Domain/Services/RepositoryServices/LayoutService.php
+++ src/Shared/Domain/Services/RepositoryServices/LayoutService.php
@@ -4,44 +4,46 @@ declare(strict_types=1);
 
 namespace Src\Shared\Domain\Services\RepositoryServices;
 
-use Src\Shared\Domain\Objects\DataObjects\UserInfoDo;
+use Thehouseofel\Hexagonal\Domain\Contracts\Services\LayoutServiceContract;
+use Thehouseofel\Hexagonal\Domain\Objects\Collections\Layout\NavbarItemCollection;
+use Thehouseofel\Hexagonal\Domain\Objects\DataObjects\Layout\UserInfoDo;
 
-final class LayoutService
+final class LayoutService implements LayoutServiceContract
 {
     public function getMessageCounter(): int
     {
         return 4;
     }
 
-    public function getNavbarNotifications()
+    public function getNavbarNotifications(): NavbarItemCollection
     {
-        return [
+        return NavbarItemCollection::fromArray([
             [
-                'icon'  => 'icon.user',
+                'icon'  => 'hexagonal::icon.user',
                 'text'  => 'New message from Bonnie Green: "Hey, what\'s up? All set for the presentation?"',
                 'time'  => 'a few moments ago',
             ],
             [
-                'icon'  => 'icon.user',
+                'icon'  => 'hexagonal::icon.user',
                 'text'  => 'Jese leos and 5 others started following you.',
                 'time'  => '10 minutes ago',
             ],
             [
-                'icon'  => 'icon.user',
+                'icon'  => 'hexagonal::icon.user',
                 'text'  => 'Joseph Mcfall and 141 others love your story. See it and view more stories.',
                 'time'  => '44 minutes ago',
             ],
             [
-                'icon'  => 'icon.user',
+                'icon'  => 'hexagonal::icon.user',
                 'text'  => 'Leslie Livingston mentioned you in a comment: @bonnie.green what do you say?',
                 'time'  => '1 hour ago',
             ],
             [
-                'icon'  => 'icon.user',
+                'icon'  => 'hexagonal::icon.user',
                 'text'  => 'Robert Brown posted a new video: Glassmorphism - learn how to implement the new design trend.',
                 'time'  => '3 hours ago',
             ],
-        ];
+        ]);
     }
 
     public function getUserInfo(): UserInfoDo
