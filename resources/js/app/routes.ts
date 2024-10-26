69dc021364a1979a8c79f04011cec15bc9c02469
Adrian Canals <a.canals14@gmail.com>
2024-10-26 05:30:19 +0200

    mover vistas de ejemplo al paquete 02: eliminar "SharedController.ts" y dejar solo un index vacío

--- resources/js/app/routes.ts
+++ resources/js/app/routes.ts
@@ -1,8 +1,6 @@
 import { Route } from 'laravel-ts-utilities';
 import HomeController from '../src/home/infrastructure/HomeController';
-import SharedController from "../src/shared/infrastructure/SharedController";
 
 export function defineRoutes(): void {
-    Route.page('shared.compare', [SharedController, 'compare']);
-    Route.page('home.example1', [HomeController, 'example1']);
+    Route.page('home.index', [HomeController, 'index']);
 }
