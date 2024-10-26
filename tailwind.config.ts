db0230e49940601e4a4f17bbdb2ff0f7116b84a2
Adrian Canals <a.canals14@gmail.com>
2024-10-26 05:20:51 +0200

    mover configuración inicial "tailwind.config.ts" al paquete "laravel-ts-utilities"

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,28 +1,16 @@
 import type { Config } from 'tailwindcss'
-import plugin from 'tailwindcss/plugin';
+import { laravelContent, laravelDefaultPlugins } from 'laravel-ts-utilities/dist/plugins';
 
 export default {
   content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.ts",
-    "./resources/**/*.vue",
-    "./node_modules/flowbite/**/*.js",
-    "./vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.js",
-    "./vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.blade.php",
+    ...laravelContent
   ],
   theme: {
     extend: {
-      screens: {
-        'vsm': '440px'
-      }
+      screens: {}
     },
   },
   plugins: [
-    require('flowbite/plugin'),
-    plugin(function ({ addVariant }) {
-      addVariant('sc', '&:is(.sc *)');
-    })
+    ...laravelDefaultPlugins
   ],
 } satisfies Config
-
