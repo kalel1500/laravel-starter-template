27672a9a5bdbb3d18e9bcd106b879fb5d7832840
Adrián <acanals@externos.itnow.es>
2024-10-26 05:30:19 +0200

    mover vistas de ejemplo al paquete 02: eliminar "SharedController.ts" y dejar solo un index vacío

--- resources/js/src/home/infrastructure/HomeController.ts
+++ resources/js/src/home/infrastructure/HomeController.ts
@@ -1,9 +1,7 @@
-import { _const } from '../../../app/constants';
-import {g} from "laravel-ts-utilities";
+import { g } from 'laravel-ts-utilities';
 
 export default class HomeController {
-    example1() {
-        console.log(_const('VITE_BROADCASTING_ENABLED'));
-        g.consoleInfo('ss');
+    index() {
+        g.consoleInfo('Inicio');
     }
 }
