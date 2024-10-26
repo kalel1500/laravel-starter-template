e425c1533fed4261db258154338f7ed092980da1
Adrián <acanals@externos.itnow.es>
2024-10-26 05:23:24 +0200

    mover vistas de ejemplo al paquete 01: eliminar ejemplos y dejar solo un index vacío

---
+++ resources/views/pages/home/index.blade.php
@@ -0,0 +1,9 @@
+<x-hexagonal::layout.app title="Test">
+
+    <x-hexagonal::section>
+
+        <div></div>
+
+    </x-hexagonal::section>
+
+</x-hexagonal::layout.app>
--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Src\Home\Infrastructure\HomeController;
 
-Route::get('/', fn() => redirect()->route('home.example0'));
+Route::get('/', fn() => redirect()->route('home'));
 
-Route::get('/compare',       [\Src\Shared\Infrastructure\TestController::class, 'compare'])->name('shared.compare');
-Route::get('/home/example0', [\Src\Home\Infrastructure\HomeController::class, 'example0'])->name('home.example0');
-Route::get('/home/example1', [\Src\Home\Infrastructure\HomeController::class, 'example1'])->name('home.example1');
-Route::get('/home/example2', [\Src\Home\Infrastructure\HomeController::class, 'example2'])->name('home.example2');
-Route::get('/home/example3', [\Src\Home\Infrastructure\HomeController::class, 'example3'])->name('home.example3');
+Route::get('/home', [HomeController::class, 'index'])->name('home');
