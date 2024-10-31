10380f1f47ea5fb941aadedfdc44be65a967b03c
Adrian Canals <a.canals14@gmail.com>
2024-10-31 15:34:14 +0100

    simplificar: comentar "laravelTsUtilsPlugin()" en el archivo "vite.config.ts"

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-import { laravelTsUtilsPlugin } from 'laravel-ts-utilities/dist/plugins';
+// import { laravelTsUtilsPlugin } from 'laravel-ts-utilities/dist/plugins';
 
 export default defineConfig({
     plugins: [
@@ -8,6 +8,6 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.ts'],
             refresh: false,
         }),
-        laravelTsUtilsPlugin()
+        // laravelTsUtilsPlugin()
     ],
 });
