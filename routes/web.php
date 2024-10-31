9bdddd23fa6f6d96b56a0e1f4b03293e82147a12
Adrián <acanals@externos.itnow.es>
2024-10-31 22:07:54 +0100

    igualar-instalacion-limpia: + Modelo User + Migraciones iniciales (users, cache y jobs) + Vista "welcome.blade.php" (en la ruta "/welcome" en lugar de "/") + Archivos actualizados a Laravel 11.30 (".env.example", ".gitignore", "composer.json", "package.json" y "postcss.config.js")

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use Src\Home\Infrastructure\HomeController;
 
+Route::get('/welcome', fn() => view('welcome'));
 Route::get('/', fn() => redirect()->route('home'));
-
 Route::get('/home', [HomeController::class, 'index'])->name('home');
