4409f1d4d2dc54257cd4b21f8a980997485a37b7
Adrian Canals <a.canals14@gmail.com>
2024-11-04 21:40:10 +0100

    Añadir contenido inicial en la configuración de tailwind ("tailwind.config.js") (según la docu: https://tailwindcss.com/docs/guides/laravel)

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,9 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
-
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
         extend: {
