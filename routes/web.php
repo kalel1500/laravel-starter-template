8cc00e4e4d7bd197deb13251f7b39e32650cd8ee
Adrian Canals <a.canals14@gmail.com>
2024-11-07 19:40:04 +0100

    Añadir comentarios a las rutas

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,13 @@
 use Illuminate\Support\Facades\Route;
 use Src\Home\Infrastructure\HomeController;
 
+/**
+ * Ruta original de Laravel para la vista welcome
+ */
 Route::get('/welcome', fn() => view('welcome'));
+
+/**
+ * Rutas de la aplicación
+ */
 Route::get('/', fn() => redirect()->route('home'));
 Route::get('/home', [HomeController::class, 'index'])->name('home');
