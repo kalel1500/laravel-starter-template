f2a127ae8e1d2fbcebf599711605cac75a3cf313
Adrián <acanals@externos.itnow.es>
2024-11-07 19:41:55 +0100

    Añadir código mínimo en "bootstrap.js" y "tailwind.config.js" para que funcione ruta "home"

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,4 +1,6 @@
-import axios from 'axios';
+import axios from "axios";
+import "flowbite";
+
 window.axios = axios;
 
-window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common["X-Requested-With"] = "XMLHttpRequest";
