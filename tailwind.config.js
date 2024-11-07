63392be34f128d14468f6305abca74a51f7cb27e
Adrian Canals <a.canals14@gmail.com>
2024-11-07 19:41:55 +0100

    Añadir código mínimo en "bootstrap.js" y "tailwind.config.js" para que funcione ruta "home"

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,11 +6,18 @@ export default {
         './resources/**/*.blade.php',
         './resources/**/*.js',
         './resources/**/*.vue',
+        './node_modules/flowbite/**/*.js',
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.js',
+        './vendor/kalel1500/laravel-hexagonal-and-ddd-architecture-utilities/resources/**/*.blade.php',
     ],
     theme: {
         extend: {
             fontFamily: {
                 sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+            screens: {
+                'vsm': '440px',
             },
         },
     },
