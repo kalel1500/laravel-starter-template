4af858f5c2ace085cb56bc88e931c29eda078a5d
Adrian Canals <a.canals14@gmail.com>
2024-11-08 22:46:28 +0100

    igualar-instalacion-limpia: vaciar "app/Providers/AppServiceProvider.php"

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,9 +2,6 @@
 
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -14,7 +11,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // HexagonalService::ignoreMigrations();
+        //
     }
 
     /**
@@ -22,9 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-//        RateLimiter::for('startAsyncAction', function (Request $request) {
-//            return Limit::perMinute(1)->by($request->path());
-//            // return $request->is('*/status/*') ? Limit::perMinute(1)->by($request->path()) : Limit::none();
-//        });
+        //
     }
 }
