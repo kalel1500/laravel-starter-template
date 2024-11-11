9f86c8755d46bda6095bdeb1558a15bd2705263e
Adrian Canals <a.canals14@gmail.com>
2024-11-11 08:47:53 +0100

    igualar-instalacion-limpia: "tailwind.config.js"

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,11 @@ import defaultTheme from 'tailwindcss/defaultTheme';
 /** @type {import('tailwindcss').Config} */
 export default {
     content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
         './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
     ],
     theme: {
         extend: {
